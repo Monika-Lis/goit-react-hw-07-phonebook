9b437f5f2cd9c5113bbd5f2750d6935207593687
Monika-Lis <144853392+Monika-Lis@users.noreply.github.com>
2024-05-28 20:41:01 +0100

    Initial commit

---
+++ src/components/Sections/Contacts.jsx
@@ -0,0 +1,47 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'components/Redux/ContactSlice';
+import css from '../Styles/Contacts.module.css';
+import PropTypes from 'prop-types';
+
+export const Contacts = () => {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name?.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <table className={css.table}>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredContacts.map(contact => (
+            <tr className={css.trBody} key={contact.id}>
+              <td>{contact.name}: </td>
+              <td>{contact.number}</td>
+              <td className={css.tdButton}>
+                <button
+                  className={css.button}
+                  onClick={() => dispatch(deleteContact(contact.id))}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Contacts.propTypes = {
+  contacts: PropTypes.array,
+  filter: PropTypes.string,
+};
---
+++ src/components/Sections/Input.jsx
@@ -0,0 +1,81 @@
+import css from '../Styles/Input.module.css';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Notiflix from 'notiflix';
+import { nanoid } from 'nanoid';
+import { addContact } from 'components/Redux/ContactSlice';
+
+export const Input = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const contacts = useSelector(state => state.contacts);
+  const dispatch = useDispatch();
+
+  const handleAddName = event => {
+    setName(event.target.value);
+  };
+  const handleAddNumber = event => {
+    setNumber(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(contacts);
+    const isPresent = contacts.some(
+      contact => contact.name.toLowerCase() === name.trim().toLowerCase()
+    );
+
+    if (isPresent) {
+      Notiflix.Notify.warning('This contact already exists!');
+      return;
+    }
+    dispatch(addContact({ id: nanoid(), name, number }));
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <div className={css.div}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label className={css.label} htmlFor="nameField">
+          Name
+        </label>
+        <input
+          className={css.input}
+          id="nameField"
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleAddName}
+          placeholder="Name"
+          required
+        />
+        <label className={css.label} htmlFor="phoneField">
+          Number
+        </label>
+        <input
+          className={css.input}
+          id="phoneField"
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleAddNumber}
+          placeholder="Phone Number"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          pattern="\+?\d{1,4}?[ .\\-\\s]?\(?\d{1,3}?\)?[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,9}"
+          required
+        />
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Input.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.string,
+};
